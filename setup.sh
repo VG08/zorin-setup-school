61d25ce4b19d849f7478bc21af05222c3c5cc196
VG <71305756+VG08@users.noreply.github.com>
2022-07-30 22:08:03 +0530

    intial script

---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo apt update && sudo apt upgrade
+sudo mv /usr/bin/xfdesktop-settings /root/
+sudo mv /usr/bin/zorin-appearance /root/
+sudo mv /usr/share/applications/zorin-appearance.desktop /root/
+sudo rm -rf .config/zorin
